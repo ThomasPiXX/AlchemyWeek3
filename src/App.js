02261da7b109d1b3f5471437d63f6087304e4019
Tom Pivin <thomas.pixx90@gmail.com>
2023-11-10 21:25:14 -0500

    adding feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Alchemy, Network } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
+import {ethers} from 'ethers';
+
 
 import './App.css';
 
@@ -21,16 +23,93 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [publicKeyBalance, setPublicKeyBalance] = useState('');
+  const [publicKeyTransaction, setPublicKeyTransaction] = useState('');
+  const [balance, setBalance] = useState(null);
+  const [transactions,  setTransaction] = useState(null);
 
   useEffect(() => {
     async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+      try{
+        const response = await alchemy.core.getBlockNumber();
+        console.log('block Number Response:', response);
+        setBlockNumber(response);
+      } catch (error) {
+        console.error('error retrieving block number:', error)
+      }
     }
-
     getBlockNumber();
-  });
+  },[]);
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  const handleInputChange = (e) => {
+    setPublicKeyBalance(e.target.value);
+  }
+
+  const handleSubmit1 = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await alchemy.core.getBalance(publicKeyBalance);
+  
+      if (response && response._hex) {
+        const balanceValue = response._hex;
+  
+        const formattedBalance = ethers.formatUnits(balanceValue, 18);
+  
+        setBalance(formattedBalance);
+      } else {
+        console.error('Invalid balance response:', response);
+      }
+    } catch (error) {
+      console.error('Error retrieving balance:', error);
+    }
+  };
+  const handleInputChange2 = (e) => {
+    setPublicKeyTransaction(e.target.value);
+  }
+
+  const handleSubmit2 = async (e) => {
+    e.preventDefault();
+    try{
+      const transactions = await alchemy.core.getTransactionCount(publicKeyTransaction);
+
+      setTransaction(transactions);
+    }catch (error) {
+      console.error("error retrieving transactions:", error.message);
+      throw error;
+    }
+  }
+
+  return ( 
+  <div className="App">
+  <div>Block Number: {blockNumber}/</div>
+  
+  <form onSubmit={handleSubmit1}>
+    <label>
+      Enter your wallet public key:
+      <input
+      type = "text"
+      value ={publicKeyBalance}
+      onChange={handleInputChange}
+      />
+    </label>
+    <button type="submit">Get Balance</button>
+  </form>
+    {balance && <div>Balance: {balance}</div>}
+
+
+    <form onSubmit={handleSubmit2}>
+        <label>Enter PublicKey to know how much transactions that account have executed</label>
+        <input type="text" value={publicKeyTransaction} onChange={handleInputChange2} />
+        <button type="submit">Get Transactions</button>
+      </form>
+      {transactions && (
+        <div>
+          Transaction:
+          <pre>{JSON.stringify(transactions, null, 2)}</pre>
+        </div>
+      )}
+    </div>      
+  );
 }
 
 export default App;
